3a55876961c978dcfc965a242de284396a17e63c
Yana Pechenenko <pechenenko.ya@gmail.com>
2025-05-20 12:21:23 +0300

    fix render on home page

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,11 +1,15 @@
 import HeroImage from "../../components/HeroImage/HeroImage";
 import MainInfo from "../../components/MainInfo/MainInfo";
 import MainStatistic from "../../components/MainStatistic/MainStatistic";
+import { useAppSelector } from "../../redux/hook";
+import { selectTheme } from "../../redux/theme/slice";
 import s from "./HomePage.module.css";
 
 const HomePage = () => {
+  const theme = useAppSelector(selectTheme);
+
   return (
-    <main>
+    <main key={theme}>
       <div className={s.wrap}>
         <MainInfo />
         <HeroImage />
